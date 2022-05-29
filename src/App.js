e571a98841d117b213550ef7a471e3598ffd6f76
Zak Stone <zrlstone@gmail.com>
2022-05-29 15:10:02 +0100

    Add specific time

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import CountdownTimer from './CountdownTimer';
 import './App.css';
 
 export default function App() {
-  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
-  const NOW_IN_MS = new Date().getTime();
+  // const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const targetTameDate = new Date('August 25, 2022 15:00:00').getTime();
 
-  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
+  // const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
 
   return (
     <div>
       <div>
         <h1 class='header'>Tame Impala Countdown Timer</h1>
       </div>
-      <CountdownTimer targetDate={dateTimeAfterThreeDays} />
+      <CountdownTimer targetDate={targetTameDate} />
     </div>
   );
 }
